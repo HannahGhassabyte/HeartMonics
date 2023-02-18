80ceea18ca4f94d98449034fdb2a709351657537
HannahGhassabyte <hghassabeh@gmail.com>
2023-02-18 18:22:31 -0500

    modded the led current

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import max30100
 
-mx30 = max30100.MAX30100()
+mx30 = max30100.MAX30100(led_current_ir=7.6)
 mx30.enable_spo2()
 
 while 1:
@@ -9,12 +9,10 @@ while 1:
 
     mx30.ir, mx30.red
 
-    hb = int(mx30.ir / 100)
+    hb = int(mx30.ir / 100 )
     spo2 = int(mx30.red / 100)
     
     if mx30.ir != mx30.buffer_ir :
         print("Pulse:",hb);
     if mx30.red != mx30.buffer_red:
         print("SPO2:",spo2);
-
-    time.sleep(2)
