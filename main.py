1cf5e9c8a43dd58c76fa9888dc2e7f26277965d7
HannahGhassabyte <hghassabeh@gmail.com>
2023-02-18 18:24:12 -0500

    added GPIO set up

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import max30100
 mx30 = max30100.MAX30100(led_current_ir=7.6)
 mx30.enable_spo2()
 
+GPIO.setmode (GPIO.BCM)
+GPIO.setwarnings (False)
+GPIO.setup (17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
 while 1:
     mx30.read_sensor()
 
@@ -13,6 +18,6 @@ while 1:
     spo2 = int(mx30.red / 100)
     
     if mx30.ir != mx30.buffer_ir :
-        print("Pulse:",hb);
+        print("Pulse:",hb)
     if mx30.red != mx30.buffer_red:
-        print("SPO2:",spo2);
+        print("SPO2:",spo2)
