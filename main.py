740b031b0a3472e277315259e3064f9e96dcadf4
HannahGhassabyte <hghassabeh@gmail.com>
2023-02-18 20:47:10 -0500

    adding led

--- main.py
+++ main.py
@@ -5,38 +5,51 @@ import sys
 import RPi.GPIO as GPIO
 
 BUTTON_GPIO = 17
+BLUE_GPIO = 10 
+GREEN_GPIO = 9 
+RED_GPIO = 11
+LED_HZ = 75
+
 LED_CURRENT_IR = 7.6
 
+# IRQ
 def button_pressed_callback(GPIO_pin_interrupted):
     print("Button pressed")
 
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)    
 
-
-
+# Intialze perpherials
 mx30 = max30100.MAX30100(led_current_ir = LED_CURRENT_IR)
 mx30.enable_spo2()
 
+# button setup
 GPIO.setmode (GPIO.BCM)
 GPIO.setwarnings (False)
 GPIO.setup (BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#rgb led setup 
+GPIO.setup(BLUE_GPIO, GPIO.OUT)
+GPIO.setup(GREEN_GPIO, GPIO.OUT)
+GPIO.setup(RED_GPIO, GPIO.OUT)
+
+# create objects from pwm 
+blue = GPIO.PWN(BLUE_GPIO, LED_HZ)
+green = GPIO.PWN(GREEN_GPIO, LED_HZ)
+red = GPIO(RED_GPIO, LED_HZ)
 
 # Enable Interrupts
 GPIO.add_event_detect(BUTTON_GPIO, GPIO.RISING, callback=button_pressed_callback, bouncetime=200)
 
 while 1:
-    mx30.read_sensor()
-
-    mx30.ir, mx30.red
-
-    hb = int(mx30.ir / 100 )
-    spo2 = int(mx30.red / 100)
     
+    # Read the heart rate 
+    mx30.read_sensor()
+    mx30.ir
+    hb = int(mx30.ir / 100 )
     if mx30.ir != mx30.buffer_ir :
-        print("Pulse:",hb)
-    if mx30.red != mx30.buffer_red:
-        print("SPO2:",spo2)
+        print("Pulse:", hb)
+    
+    # Update led
+    red.start(255/2.5)
+     
     
     time.sleep(1)
